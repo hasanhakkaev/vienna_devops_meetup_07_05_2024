be41d6660d17f256176c82bb16faf7733ebb2d4e
Hasan Hakkaev <hasan.hakkaev@gmx.at>
2024-04-30 09:46:49 +0200

    policy change, new  endpoint and task fixes

--- app/main.go
+++ app/main.go
@@ -7,14 +7,20 @@ import (
 	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Vienna DevOps Meetup - %s", time.Now().Local())
-}
-
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/time", timeHandler)
 	fmt.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", logRequest(http.DefaultServeMux)))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Vienna DevOps Meetup")
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Fprintf(w, "Current time: %s", currentTime)
 }
 
 func logRequest(handler http.Handler) http.Handler {
